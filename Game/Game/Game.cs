18a92985ebb9944cfab08dbd487acf131969ddae
KirillBorisovich <116075864+KirillBorisovich@users.noreply.github.com>
2025-03-28 16:16:17 +0300

    feat: did class Game

---
+++ Game/Game/Game.cs
@@ -0,0 +1,13 @@
+﻿namespace Game;
+
+public class Game
+{
+    public void OnLeft(object sender, EventArgs args)
+    => Console.WriteLine("Going left");
+    public void OnRight(object sender, EventArgs args)
+    => Console.WriteLine("Going right");
+    public void withdrawMap()
+    {
+
+    }
+}

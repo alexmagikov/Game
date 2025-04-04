a212e3a471ce0465c7699868f97d9bc786f858b1
KirillBorisovich <116075864+KirillBorisovich@users.noreply.github.com>
2025-04-04 16:38:38 +0300

    feat: transferred the game code

---
+++ WinFormsApp1/WinFormsApp1/EventLoop.cs
@@ -0,0 +1,32 @@
+﻿namespace WinFormsApp1;
+
+public class EventLoop
+{
+    public event EventHandler<EventArgs> LeftHendler = (sender, args) => { };
+    public event EventHandler<EventArgs> RightHendler = (sender, args) => { };
+    public event EventHandler<EventArgs> UpHendler = (sender, args) => { };
+    public event EventHandler<EventArgs> DownHendler = (sender, args) => { };
+    public event EventHandler<EventArgs> MapHendler = (sender, args) => { };
+    public void Run()
+    {
+        while (true)
+        {
+            var key = Console.ReadKey(true);
+            switch (key.Key)
+            {
+                case ConsoleKey.LeftArrow:
+                    LeftHendler(this, EventArgs.Empty);
+                    break;
+                case ConsoleKey.RightArrow:
+                    RightHendler(this, EventArgs.Empty);
+                    break;
+                case ConsoleKey.UpArrow:
+                    UpHendler(this, EventArgs.Empty);
+                    break;
+                case ConsoleKey.DownArrow:
+                    DownHendler(this, EventArgs.Empty);
+                    break;
+            }
+        }
+    }
+}

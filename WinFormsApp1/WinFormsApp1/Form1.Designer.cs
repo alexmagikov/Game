c923b693127184d81463e49d16b05e91a50bff34
alexmagikov <alexmagikov228@gmail.com>
2025-04-04 16:13:01 +0300

    add winform app

---
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            TestButton = new Button();
+            SuspendLayout();
+            // 
+            // TestButton
+            // 
+            TestButton.BackColor = SystemColors.MenuHighlight;
+            TestButton.Location = new Point(359, 214);
+            TestButton.Name = "TestButton";
+            TestButton.Size = new Size(112, 34);
+            TestButton.TabIndex = 0;
+            TestButton.Text = "button1";
+            TestButton.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(TestButton);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button TestButton;
+    }
+}

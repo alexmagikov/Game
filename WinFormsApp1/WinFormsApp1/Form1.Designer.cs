e045ee3304d9727c823dd5aaa13da524655e6f61
alexmagikov <alexmagikov228@gmail.com>
2025-04-04 16:39:11 +0300

    add doom

--- WinFormsApp1/WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -28,32 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            TestButton = new Button();
             SuspendLayout();
-            // 
-            // TestButton
-            // 
-            TestButton.BackColor = SystemColors.MenuHighlight;
-            TestButton.Location = new Point(359, 214);
-            TestButton.Name = "TestButton";
-            TestButton.Size = new Size(112, 34);
-            TestButton.TabIndex = 0;
-            TestButton.Text = "button1";
-            TestButton.UseVisualStyleBackColor = false;
+        
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(TestButton);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
         }
 
         #endregion
-
-        private Button TestButton;
     }
 }
